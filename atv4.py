000f76247be4dafaad121b314c33f306d6127638
Querubinho <97846266+Querubinho@users.noreply.github.com>
2025-03-28 20:19:01 -0300

    Update atv4.py

--- atv4.py
+++ atv4.py
@@ -1,15 +1,14 @@
-#Um cliente alugou um carro e rodou alguns quilômetros por um certo número de
-#dias. O custo diário é de R$90. Se ele rodou mais de 100 km, há uma taxa extra de
-#R$12 por km adicional. Calcule o valor total a ser pago.
+# Erick Raphael - 3º ano B  
+# Atividade para a professora Letícia  
 
-distancia = int(input("Diga por quantos km's voce andou:"))
+distancia = int(input("Diga quantos quilometros voce andou:"))
 tempo = int(input("Diga por quantos dias voce alugou o carro:"))
 
 
 if distancia > 100:
  taxa = (distancia - 100) * 12
  valor = tempo * 90 + taxa
- print(f"Quilometragem acima de 100, uma taxa foi aplicada. Valor: {valor}")
+ print(f"Quilometragem acima de 100, taxa adicionada ao valor. Total: {valor}")
 
 else:
  valor = tempo * 90
