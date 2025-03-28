e6d4dfe64454bfb9dc8dd4904abdfb56efd0d000
Querubinho <97846266+Querubinho@users.noreply.github.com>
2025-03-28 20:17:48 -0300

    Update atv3.py

--- atv3.py
+++ atv3.py
@@ -1,8 +1,7 @@
-#Peça ao usuário o valor total das mercadorias compradas. Se for menor que R$500,
-#não há imposto. Caso contrário, aplique uma taxa de 50% sobre o valor que ultrapassar
-#os R$500.
+# Erick Raphael - 3º ano B  
+# Atividade para a professora Letícia  
 
-valor = int(input("Diga o valor das compras:"))
+valor = int(input("Diga o valor total das compras:"))
 
 if valor > 500:
     taxa = (valor - 500) * 1.5
